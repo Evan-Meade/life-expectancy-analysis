76a6e32f9e5d2f32797b2cf578d59feeeb80035b
Evan Meade <evan.meade@protonmail.com>
2021-05-09 17:50:03 -0500

    Comments and model update

--- evan-analysis.r
+++ evan-analysis.r
@@ -1,71 +1,54 @@
----
-title: "Evan Analysis"
-author: "Evan Meade"
-date: "4/26/2021"
-output:
-  html_fragment:
-    self_contained: no
----
+# Evan Meade, 2021
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
+# 
+# Begin Evan
+# 
 
-## Summary
-
-* Developed countries have 12 year higher life expectancies than developing countries, on average
-* Average national life expectancy is increasing at about a third of a year per year 
-* Errors in the 9-parameter model are approximately normal
-* Average errors for each country (averaged over time) are also approximately normal
-* There are outliers over- and under-performing on average over this time period
-
-Next steps:
-
-* Statistically test for significance of these over- and under-performers
-* Try adding Status.proxy and Year as parameters to the model for increased accuracy
-
-## Imports
-
-```{r imports}
+# Library imports
 library(tidyverse)
 library(car)
 
+# Reading in the cleaned data
 data <- read_csv("life_CLEAN.csv")
 data
-```
 
-## Linear Test
 
-```{r linear_test}
+# 
+# Question 2 - Evan
+#
+
+# Plotting each predictor against life expectancy
 data %>% gather(Predictor, x, Schooling:thinness.10.19.years) %>%
   ggplot() +
-    geom_point(aes(x, Life.expectancy, color = Predictor), alpha = 0.1) +
-    geom_smooth(aes(x, Life.expectancy, group = Predictor),
-                color = "black", method = "lm", se = FALSE) +
-    facet_wrap(~ Predictor, scales = "free_x") +
-    theme(legend.position = "none") +
-    labs(title = "Scatter Plots of Life Expectancy vs. Each Predictor")
-```
+  geom_point(aes(x, Life.expectancy, color = Predictor), alpha = 0.1) +
+  geom_smooth(aes(x, Life.expectancy, group = Predictor),
+              color = "black", method = "lm", se = FALSE) +
+  facet_wrap(~ Predictor, scales = "free_x") +
+  theme(legend.position = "none") +
+  labs(title = "Scatter Plots of Life Expectancy vs. Each Predictor")
 
-## Temporal Analysis
+# 
+# Question 4 - Evan
+# 
 
-```{r over_time}
+# Calculating average life expectancies for each year
 year_averages <- aggregate(Life.expectancy ~ Year, data = data, FUN = mean)
 
+# Plotting average national life expectancy over time
 ggplot(data = year_averages, mapping = aes(x = Year, y = Life.expectancy)) +
   geom_line() +
   labs(title = "Average National Life Expectancy Over Time",
        x = "Year",
        y = "Average National Life Expectancy (Years)") +
   stat_smooth(method = "lm", se = FALSE, linetype = "dashed")
-# savefig("life_expectancy__year.png")
 
+# Summarizing fit to year
 summary(lm(Life.expectancy ~ Year, data = year_averages))
-```
 
-```{r status_time}
+# Average life expectancies per year, with term for development status
 status_averages <- aggregate(Life.expectancy ~ Year + Status, data = data, FUN = mean)
 
+# Plotting developed vs. developing average life expectancies over time
 ggplot(data = status_averages, mapping = aes(x = Year, y = Life.expectancy)) +
   geom_line(mapping = aes(color = Status)) +
   labs(title = "Average National Life Expectancy Over Time, By Development Status",
@@ -73,94 +56,81 @@ ggplot(data = status_averages, mapping = aes(x = Year, y = Life.expectancy)) +
        y = "Average National Life Expectancy (Years)") +
   stat_smooth(mapping = aes(group = Status), method = "lm", se = FALSE,
               color = "black", size = 0.5, linetype = "dashed")
-```
 
-```{r status_proxy}
+# Creating proxy variable for status
 data$Status.proxy <- as.integer(factor(data$Status, levels = c("Developing",
                                                                "Developed"),
                                        labels = c(0, 1))) - 1
+
+# Summary of fit to status proxy variable and year
 summary(lm(Life.expectancy ~ Status.proxy + Year, data = data))
-```
 
-So clearly country status is a large factor in life expectancy over time. On average, developed countries have life expectancies over 12 years higher than developing countries. Additionally, every year raises the global life expectancy by about a third of a year. These coefficients are highly significant and graphically the trends look relatively constant. If we extrapolate this pattern forwards, that means each person can expect to life on average, 1.5 times as long as the life expectancy when they were born. For people born around 2000, that's about 116 years old.
 
-## Model Fitting
+# 
+# Question 5 - Evan
+# 
 
-```{r model_fit}
+# Fitting full model to the 7 selected predictors
 model <- lm(Life.expectancy ~ Schooling + GDP + Alcohol + BMI +
               percentage.expenditure + Income.composition.of.resources +
               HIV.AIDS + thinness.5.9.years + thinness.10.19.years, data = data)
 summary(model)
 
-# Refining that to significance
+# Refining that model to significance
+# Removing thinness.5.9.years due to high colinearity colinearity
 vif(model)
 model <- lm(Life.expectancy ~ Schooling + GDP + Alcohol + BMI +
               percentage.expenditure + Income.composition.of.resources +
               HIV.AIDS + thinness.10.19.years, data = data)
 summary(model)
 
+# Removing percentage expenditure for low significance
 vif(model)
 model <- lm(Life.expectancy ~ Schooling + GDP + Alcohol + BMI +
               Income.composition.of.resources +
               HIV.AIDS + thinness.10.19.years, data = data)
 summary(model)
-```
 
-## Over- and Under-Performing Countries
-
-```{r predict}
+# Calculating predicted values and residuals for all entries
 data$y.hat <- predict(model, data)
 data$error <- data$Life.expectancy - data$y.hat
 
-country_subset <- c("Norway", "China", "Ghana", "Chile", "Vietnam")
-ggplot(data = data[which(data$Country %in% country_subset), ]) +
-  geom_line(mapping = aes(x = Year, y = error, color = Country))
-```
-
-```{r errors}
+# Plotting histogram of residuals
 ggplot(data = data) +
   geom_histogram(mapping = aes(x = error), binwidth = 1) +
   labs(title = "Residuals For All Life Expectancy Observations",
        x = "Residual (Years)",
        y = "Count")
 
+# Plotting boxplots of residual distributions over time
 ggplot(data = data) +
   geom_boxplot(mapping = aes(x = Year, group = Year, y = error)) +
   labs(title = "Residual Distributions Over Time",
        x = "Year",
        y = "Residual (Years)")
-```
 
-We can investigate the probability that a country would have error sample means of these magnitudes.
-
-$$
-H_0: \epsilon\sim N(0,\sigma^2)\text{ for all countries }
-\\
-H_1: \epsilon\not\sim N(0,\sigma^2)\text{ for at least one country }
-\\
-z_j = \frac{1}{n_j}\sum_{i=1}^{n_j}\epsilon = \frac{1}{n_j}\sum_{i=1}^{n_j}N(0,\sigma^2) \sim N(0,\sigma^2/n_j) \text{ for each country }j
-$$
-
-```{r over_under}
-# av_error <- aggregate(error ~ Country, data = data, FUN = mean)
+# Calculating p-value for each country's sample mean of the residual distribution,
+# assuming a null hypothesis of following N(0, sigma^2)
 rse <- summary(model)$sigma
 av_error <- data %>%
   group_by(Country) %>%
   summarize(count = n(), total_error = sum(error), mean_error = total_error / count) %>%
   mutate(p_val = pnorm(abs(total_error), mean = 0, sd = sqrt(count) * rse, lower.tail = FALSE))
 
+# Printing countries with statistically significant deviations from a mean residual
+# distribution of 0
 (signif_error <- av_error %>%
     filter(p_val < 0.001) %>%
     arrange(p_val))
 
+# Plotting histogram of each nation's average residual
 ggplot(data = av_error) +
   geom_histogram(mapping = aes(x = mean_error)) +
   labs(title = "Average Residuals of Each Nation",
        x = "Average Residual (Years)",
        y = "Count")
-```
 
-```{r outliers}
+# Identifying top over- and under-performers by mean residual
 outlier_order <- c(head(order(av_error$mean_error, decreasing = TRUE), 5),
                    tail(order(av_error$mean_error, decreasing = TRUE), 5))
 av_error$over.rank <- order(av_error$mean_error, decreasing = TRUE)
@@ -170,22 +140,14 @@ print(paste0(c("Top Under-perfomers: ", outliers[10:6])))
 outlier_data <- data[which(data$Country %in% outliers), ]
 outlier_data$Country <- factor(outlier_data$Country, levels = outliers)
 
+# Plotting top 5 over- and under-performers by mean error
 ggplot(data = outlier_data) +
   geom_line(mapping = aes(x = Year, y = error, color = Country)) +
   scale_color_brewer(palette = "RdYlGn", direction = -1) +
   labs(title = "Top Over- and Under-Performing National Life Expectancies",
        x = "Year",
        y = "Residual")
-```
 
-```{r error_signif}
-
-```
-
-
-
-
-
-
-
-
+# 
+# End Evan
+# 
